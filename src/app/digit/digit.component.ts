c2ab2c6474c1e71539ed9f9987686f94be4ecb73
skavoo <skavoo@gmail.com>
2021-06-07 22:13:09 +0200

    2 files modified

--- src/app/digit/digit.component.ts
+++ src/app/digit/digit.component.ts
@@ -42,55 +42,53 @@ export class DigitComponent implements OnChanges, OnInit {
       if (changes.numero.currentValue == null) {
         this.ngOnInit();
       } else {
-        for (const propName in changes) {
-          const change = changes[propName];
-          const to = JSON.stringify(change.currentValue);
-          const from = JSON.stringify(change.previousValue);
+        const change = changes['numero'];
+        const to = JSON.stringify(change.currentValue);
+        const from = JSON.stringify(change.previousValue);
 
-          var splitted = to.split('.');
+        var splitted = to.split('.');
 
-          // left padding
-          for (
-            var i = 0, len = this.cifreIntere - splitted[0].length;
-            i < len;
-            i += 1
-          ) {
-            this.digits.push('\u00A0\u00A0');
+        // left padding
+        for (
+          var i = 0, len = this.cifreIntere - splitted[0].length;
+          i < len;
+          i += 1
+        ) {
+          this.digits.push('\u00A0\u00A0');
+        }
+
+        // parte intera
+        for (var i = 0, len = splitted[0].length; i < len; i += 1) {
+          if (i < this.cifreIntere) {
+            this.digits.push(splitted[0].charAt(i));
           }
+        }
 
-          // parte intera
-          for (var i = 0, len = splitted[0].length; i < len; i += 1) {
-            if (i < this.cifreIntere) {
-              this.digits.push(splitted[0].charAt(i));
+        // virgola
+        this.digits.push(',');
+
+        // parte decimale
+        if (splitted[1]) {
+          for (var i = 0, len = splitted[1].length; i < len; i += 1) {
+            if (i < this.cifreDecimali) {
+              this.digits.push(splitted[1].charAt(i));
             }
           }
-
-          // virgola
-          this.digits.push(',');
-
-          // parte decimale
-          if (splitted[1]) {
-            for (var i = 0, len = splitted[1].length; i < len; i += 1) {
-              if (i < this.cifreDecimali) {
-                this.digits.push(splitted[1].charAt(i));
-              }
-            }
-            // right padding dei decimali
-            if (splitted[1].length < this.cifreDecimali) {
-              for (
-                var i = 0, len = this.cifreDecimali - splitted[1].length;
-                i < len;
-                i += 1
-              ) {
-                this.digits.push('0');
-              }
-            }
-          }
-          // se la parte decimale manca riempi di 0
-          else {
-            for (var i = 0, len = this.cifreDecimali; i < len; i += 1) {
+          // right padding dei decimali
+          if (splitted[1].length < this.cifreDecimali) {
+            for (
+              var i = 0, len = this.cifreDecimali - splitted[1].length;
+              i < len;
+              i += 1
+            ) {
               this.digits.push('0');
             }
+          }
+        }
+        // se la parte decimale manca riempi di 0
+        else {
+          for (var i = 0, len = this.cifreDecimali; i < len; i += 1) {
+            this.digits.push('0');
           }
         }
       }
