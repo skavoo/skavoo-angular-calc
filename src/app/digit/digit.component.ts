9d4618b52fd65a49821f5ca3fcc7899f8ecb3838
skavoo <skavoo@gmail.com>
2021-06-07 10:25:17 +0200

    2 files modified, 3 added, 6 deleted

--- src/app/digit/digit.component.ts
+++ src/app/digit/digit.component.ts
@@ -10,30 +10,21 @@ import {
 
 @Component({
   selector: 'digit-component',
-  template: `
-    <table class="digit-table">
-      <thead>
-        <tr>
-          <th *ngFor="let column of digits"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td *ngFor="let list of digits">
-            {{ list }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  `,
-  styleUrls: ['./app.component.css']
+  templateUrl: './digit.component.html',
+  styleUrls: ['./digit.component.css']
 })
 export class DigitComponent implements OnChanges, OnInit {
   @Input() numero: number;
   digits: string[] = [];
 
   ngOnInit() {
-    console.log('OnInit');
+    for (var j = 0, len = 7; j < len; j += 1) {
+      if (j === 4) {
+        this.digits.push(',');
+      } else {
+        this.digits.push('\u00A0\u00A0');
+      }
+    }
   }
 
   ngOnChanges(changes: SimpleChanges) {
