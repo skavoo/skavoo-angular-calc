d647e220924d97d01ceeefe3c9b3c83bbc5eb36f
skavoo <skavoo@gmail.com>
2021-06-07 15:07:36 +0200

    1 file modified

--- src/app/digit/digit.component.ts
+++ src/app/digit/digit.component.ts
@@ -17,9 +17,16 @@ export class DigitComponent implements OnChanges, OnInit {
   @Input() numero: number;
   digits: string[] = [];
 
+  cifreIntere: number = 4;
+  cifreDecimali: number = 2;
+
   ngOnInit() {
-    for (var j = 0, len = 7; j < len; j += 1) {
-      if (j === 4) {
+    for (
+      var j = 0, len = this.cifreIntere + this.cifreDecimali + 1;
+      j < len;
+      j += 1
+    ) {
+      if (j === this.cifreIntere) {
         this.digits.push(',');
       } else {
         this.digits.push('\u00A0\u00A0');
@@ -31,20 +38,58 @@ export class DigitComponent implements OnChanges, OnInit {
     this.digits = [];
     console.log(changes);
     if (!changes.numero.firstChange) {
-      for (const propName in changes) {
-        const change = changes[propName];
-        const to = JSON.stringify(change.currentValue);
-        const from = JSON.stringify(change.previousValue);
+      if (!changes.numero.currentValue) {
+        this.ngOnInit();
+      } else {
+        for (const propName in changes) {
+          const change = changes[propName];
+          const to = JSON.stringify(change.currentValue);
+          const from = JSON.stringify(change.previousValue);
 
-        for (var j = 0, len = 4 - to.length; j < len; j += 1) {
-          this.digits.push('\u00A0\u00A0');
-        }
+          var splitted = to.split('.');
 
-        for (var i = 0, len = to.length; i < len; i += 1) {
-          if (to.charAt(i) === '.') {
-            this.digits.push(',');
-          } else {
-            this.digits.push(to.charAt(i));
+          // left padding
+          for (
+            var i = 0, len = this.cifreIntere - splitted[0].length;
+            i < len;
+            i += 1
+          ) {
+            this.digits.push('\u00A0\u00A0');
+          }
+
+          // parte intera
+          for (var i = 0, len = splitted[0].length; i < len; i += 1) {
+            if (i < this.cifreIntere) {
+              this.digits.push(splitted[0].charAt(i));
+            }
+          }
+
+          // virgola
+          this.digits.push(',');
+
+          // parte decimale
+          if (splitted[1]) {
+            for (var i = 0, len = splitted[1].length; i < len; i += 1) {
+              if (i < this.cifreDecimali) {
+                this.digits.push(splitted[1].charAt(i));
+              }
+            }
+            // right padding dei decimali
+            if (splitted[1].length < this.cifreDecimali) {
+              for (
+                var i = 0, len = this.cifreDecimali - splitted[1].length;
+                i < len;
+                i += 1
+              ) {
+                this.digits.push('0');
+              }
+            }
+          }
+          // se la parte decimale manca riempi di 0
+          else {
+            for (var i = 0, len = this.cifreDecimali; i < len; i += 1) {
+              this.digits.push('0');
+            }
           }
         }
       }
